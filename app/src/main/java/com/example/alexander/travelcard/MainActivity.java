8e878395ec59df483c2ba19025bdbf00a1c2b81a
alexander <napo@itu.dk>
2017-04-05 15:02:03 +0200

    initial commit

--- app/src/main/java/com/example/alexander/travelcard/MainActivity.java
+++ app/src/main/java/com/example/alexander/travelcard/MainActivity.java
@@ -2,15 +2,57 @@ package com.example.alexander.travelcard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
 import com.estimote.sdk.SystemRequirementsChecker;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private BeaconManager beaconManager;
+    private Region region;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView) findViewById(R.id.textViewId);
+        textView.setText("What is this?");
+
+        beaconManager = new BeaconManager(this);
+        beaconManager.setRangingListener(new BeaconManager.RangingListener() {
+            @Override
+            public void onBeaconsDiscovered(Region region, List<Beacon> list) {
+                if (!list.isEmpty()) {
+                    for(Beacon beacon : list) {
+                        if(beacon.getMajor()==1729 && beacon.getMinor()==8096) {
+                            textView.setText("My Ipad");
+                        } else if(beacon.getMajor()==9842 && beacon.getMinor()==8349)
+                            textView.setText("My Iphone");
+                        else {
+                            textView.setText("Do not konw");
+                        }
+                    }
+
+                }
+            }
+        });
+        region = new Region("ranged region", UUID.fromString("8492e75f-4fd6-469d-b132-043fe94921d8"), null, null);
     }
 
 
@@ -19,5 +61,41 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
 
         SystemRequirementsChecker.checkWithDefaultDialogs(this);
+
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                beaconManager.startRanging(region);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        beaconManager.stopRanging(region);
+
+        super.onPause();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
